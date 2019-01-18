62fcd27c5d089aec4f22c964eca89057fbf078c8
Adam Wong <awong@opengov.com>
2019-01-17 21:24:48 -0800

    style

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
+
