14a3328a03d23e811b863893041edae94f7e6e4e
Adam Wong <awong@opengov.com>
2019-01-18 19:58:34 -0800

    dev server

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import _ from 'lodash';
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import _ from 'lodash';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
 import './term.jsx';
 import './style.css';
 
-function component() {
-  let element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'world'], ' ');
-  element.classList.add('hello');
+// function component() {
+//   let element = document.createElement('div');
+//   element.innerHTML = _.join(['Hello', 'world'], ' ');
+//   element.classList.add('hello');
 
-  return element;
-}
-function component2() {
-  let element = document.createElement('div');
-  element.id ='demoContainer';
+//   return element;
+// }
+// function component2() {
+//   let element = document.createElement('div');
+//   element.id ='demoContainer';
 
-  return element;
-}
-document.body.appendChild(component());
-document.body.appendChild(component2());
+//   return element;
+// }
+// document.body.appendChild(component());
+// document.body.appendChild(component2());
 
 
 
-const title = 'My Minimal React Webpack Babel Setup';
+// const title = 'My Minimal React Webpack Babel Setup';
 
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('demoContainer')
-);
+// ReactDOM.render(
+//   <div>{title}</div>,
+//   document.getElementById('demoContainer')
+// );
