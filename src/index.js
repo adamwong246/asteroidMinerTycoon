eb0dcb7b094aa26e487077c37bdbd864c6447a20
Adam Wong <awong@opengov.com>
2019-01-18 19:08:56 -0800

    jsx, babel and css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import _ from 'lodash';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './style.css';
 
 function component() {
   let element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'world'], ' ');
   element.classList.add('hello');
 
   return element;
 }
+function component2() {
+  let element = document.createElement('div');
+  element.id ='demoContainer';
 
+  return element;
+}
 document.body.appendChild(component());
+document.body.appendChild(component2());
+
+
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('demoContainer')
+);
